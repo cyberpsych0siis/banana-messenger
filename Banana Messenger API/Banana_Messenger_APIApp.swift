da5c22a9fc6fcc275c6be5e4e7318d6487007c8d
Philipp Gaßner <activator112233@gmail.com>
2022-05-09 18:00:27 +0200

    Initial Commit

---
+++ Banana Messenger API/Banana_Messenger_APIApp.swift
@@ -0,0 +1,20 @@
+//
+//  Banana_Messenger_APIApp.swift
+//  Banana Messenger API
+//
+//  Created by Philipp Gaßner on 09.05.22.
+//
+
+import SwiftUI
+
+@main
+struct Banana_Messenger_APIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
